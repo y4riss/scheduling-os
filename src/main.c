f261274d75f10322ecaeabdeacf45f6546f2bee9
joessir <lassiryyass@gmail.com>
2023-06-07 22:04:58 +0100

    added custom function to handle user input

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include "utils.h"
 
+void get_user_input(algorithm_functions *functions, int n, Processus *processus)
+{
+    char choice;
 
+    if (n == 0)
+    {
+        fprintf(stderr,"no available algorithms to execute\n");
+        exit(1);
+    }
+    while (1) {
+    printf("Algorithm > ");
+    choice = getchar();
+    if (choice == '\n' || choice == '\r') continue;
+    while(getchar() != '\n'); // To consume the newline character
+
+    if ( '1' <= choice && choice <= (n + 48) )
+        break;
+    else
+        printf("Invalid input.\n");
+    }
+    functions[choice - 48](processus);
+}
 
 
 int main(int argc, char* argv[]) {
@@ -9,29 +30,14 @@ int main(int argc, char* argv[]) {
         fprintf(stderr,"Utilisation : %s nom_fichier",argv[0]);
         return 1;
     }
-    char choice;
     int nb_of_functions;
+    algorithm_functions functions[10];
+    Processus* processus;
 
-    Processus* processus = parse_file(argv[1]);
-    algorithm_functions functions[5];
+    processus = parse_file(argv[1]);
     menu(functions,&nb_of_functions);
-    if (nb_of_functions == 0)
-    {
-        fprintf(stderr,"no available algorithms to execute\n");
-        return 1;
-    }
-    while (1) {
-    printf("Algorithm > ");
-    choice = getchar();
-    if (choice == '\n' || choice == '\r') continue;
-    while(getchar() != '\n'); // To consume the newline character
-
-    if ( '1' <= choice && choice <= (nb_of_functions + 48) )
-        break;
-    else
-        printf("Invalid input.\n");
-    }
-    functions[choice - 48](processus);
+    get_user_input(functions, nb_of_functions, processus);
+    
     free(processus);
     return 0;
 }
